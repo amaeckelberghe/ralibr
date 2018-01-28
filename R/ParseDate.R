ab3d7bc01b91cc307a7cf53b0dae41d0520a27df
THIBAUT VAN WEEHAEGHE <tvanweehaeghe@deloitte.com>
2018-01-28 21:36:11 +0100

    Updated documentation

--- R/ParseDate.R
+++ R/ParseDate.R
@@ -1,11 +1,9 @@
 ParseDate <- function(DateToParse, DateType = "European"){
-        
-        # Lubridate is dependency package in our package
-        require(lubridate)
-        
+
+
         # Supress warnings
-        options(warn=-1)
-        
+        # options(warn=-1)
+
         # Convert given the DateType input
         if(toupper(DateType)=="EUROPEAN"){
                 date <- as.Date(parse_date_time(x = DateToParse,orders = c("dmy","ymd")))
@@ -14,10 +12,10 @@ ParseDate <- function(DateToParse, DateType = "European"){
         }else{
                 stop("Wrong DateType")
         }
-        
+
         # Turn back on warnings
-        options(warn=0)
-        
+        # options(warn=0)
+
         # Output warning in case we return NA value
         if(any(is.na(x = date))){warning("ParseDate did not return a Date")}
 
